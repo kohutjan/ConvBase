3496277c9ffb89625c215c10758380c1ed75da35
Jan Kohut <kohut.honza@seznam.cz>
2017-10-15 23:21:20 +0200

    Tmp push

--- include/solver.hpp
+++ include/solver.hpp
@@ -7,15 +7,34 @@
 class Solver
 {
   public:
-    Solver(Loader * _loader, Net _net) : loader(_loader), net(_net){}
+    Solver(Loader * _loader, Net _net, float _learningRate, int _trainIterations,
+           int _testInterval, int _testIterations ) : loader(_loader), net(_net),
+           learningRate(_learningRate), trainIterations(_trainIterations),
+           testInterval(_testInterval), testIterations(_testIterations)
+    {
+      for (auto& op: this->net.operators)
+      {
+        if (op->GetType() == string("SoftmaxCrossEntropy"))
+        {
+          outputBottomName = op->GetBottomName()[0];
+          outputTopName = op->GetTopName()[0];
+        }
+      }
+    }
     std::pair<Tensor4D, Tensor4D> GetRandomTrainBatch();
     std::pair<Tensor4D, Tensor4D> GetBatch(std::string datasetType,
                                            std::vector<int> randomIndexes);
-    void Solve(int numberOfIterations, float learningRate);
+    void Solve();
     ~Solver(){}
   private:
     Loader * loader;
     Net net;
+    const float learningRate;
+    const int trainIterations;
+    const int testInterval;
+    const int testIterations;
+    string outputBottomName;
+    string outputTopName;
 };
 
 #endif
--- src/solver.cpp
+++ src/solver.cpp
@@ -2,44 +2,56 @@
 
 using namespace std;
 
-void Solver::Solve(int numberOfIterations, float learningRate)
+void Solver::Solve()
 {
   int rightGuess = 0;
   for (int n = 0; n < numberOfIterations; ++n)
   {
     pair<Tensor4D, Tensor4D> batch = this->GetRandomTrainBatch();
+    Tensor4D top = this->net.
     this->net.AddTensor4DToContainer(this->net.inputs.begin()->first, batch.second);
-    this->net.AddTensor4DToContainer("loss", batch.first);
     this->net.Forward();
-    Tensor4D top = this->net.GetTensor4DFromContainer("top");
-    float * labelVal = top.GetData();
-    for (int j = 0; j < top.GetShape()[Nd]; ++j)
-    {
-      float maxLabelVal = labelVal[j * top.GetShape()[Cd]];
-      int topLabel = 0;
-      for (int i = 1; i < top.GetShape()[Cd]; ++i)
-      {
-        if (labelVal[top.GetShape()[Cd] * j + i] > maxLabelVal)
-        {
-          maxLabelVal = labelVal[top.GetShape()[Cd] * j + i];
-          topLabel = i;
-        }
-      }
-      if (topLabel == int(batch.first.GetGradients()[j]))
-      {
-        rightGuess++;
-      }
-    }
-    if (n != 0)
-    {
-      if (n % 50 == 0)
-      {
-        cout << "Ieration " << n << " | train accuracy: " << float(rightGuess) / float(n * this->net.inputs.begin()->second[Nd]) << endl;;
-      }
-    }
+    rightGuesses += this->GetRightGuesses(batch, top);
+    this->PrintAccuracy(n, interval, rightGuesses);
     this->net.Backward();
     this->net.UpdateWeights(learningRate);
   }
+}
+
+void Solver::PrintAccuracy(int n, int interval, int rightGuesses)
+{
+  if (n != 0)
+  {
+    if (n % interval == 0)
+    {
+      cout << "Iteration " << n << " | train accuracy: " << float(rightGuesses)
+           / float(n * this->net.inputs.begin()->second[Nd]) << endl;
+    }
+  }
+}
+
+int Solver::GetRightGuesses(pair<Tensor4D, Tensor4D> batch, Tensor4D top)
+{
+  int rightGuess = 0;
+  float * labelVal = top.GetData();
+  for (int j = 0; j < top.GetShape()[Nd]; ++j)
+  {
+    float maxLabelVal = labelVal[j * top.GetShape()[Cd]];
+    int topLabel = 0;
+    for (int i = 1; i < top.GetShape()[Cd]; ++i)
+    {
+      if (labelVal[top.GetShape()[Cd] * j + i] > maxLabelVal)
+      {
+        maxLabelVal = labelVal[top.GetShape()[Cd] * j + i];
+        topLabel = i;
+      }
+    }
+    if (topLabel == int(batch.first.GetGradients()[j]))
+    {
+      rightGuess++;
+    }
+  }
+  return rightGuess;
 }
 
 pair<Tensor4D, Tensor4D> Solver::GetRandomTrainBatch()
@@ -55,25 +67,25 @@ pair<Tensor4D, Tensor4D> Solver::GetRandomTrainBatch()
   return this->GetBatch("train", randomIndexes);
 }
 
-pair<Tensor4D, Tensor4D> Solver::GetBatch(string datasetType, vector<int> randomIndexes)
+pair<Tensor4D, Tensor4D> Solver::GetBatch(string datasetType, vector<int> indexes)
 {
   vector<int> inputShape = this->net.inputs.begin()->second;
   Tensor4D labels(inputShape[Nd], 1, 1, 1);
   Tensor4D data(inputShape[Nd], inputShape[Hd], inputShape[Wd], inputShape[Cd]);
   float * labelsVal = labels.GetGradients();
   float * dataVal = data.GetData();
-  for (int i = 0; i < randomIndexes.size(); ++i)
+  for (int i = 0; i < indexes.size(); ++i)
   {
     vector<float> randomImage;
     if (datasetType == string("train"))
     {
-      labelsVal[i] = this->loader->trainDataset.first[randomIndexes[i]];
-      randomImage = this->loader->trainDataset.second[randomIndexes[i]];
+      labelsVal[i] = this->loader->trainDataset.first[indexes[i]];
+      randomImage = this->loader->trainDataset.second[indexes[i]];
     }
     else
     {
-      labelsVal[i] = this->loader->testDataset.first[randomIndexes[i]];
-      randomImage = this->loader->testDataset.second[randomIndexes[i]];
+      labelsVal[i] = this->loader->testDataset.first[indexes[i]];
+      randomImage = this->loader->testDataset.second[indexes[i]];
     }
     memcpy(dataVal + i * inputShape[Hd] * inputShape[Wd] * inputShape[Cd],
            &randomImage[0], inputShape[Hd] * inputShape[Wd] * inputShape[Cd]
